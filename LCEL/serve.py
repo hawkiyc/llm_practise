6a4dcce136ee7c8a603308c335855d9b61798350
Gavin Jang <hawkiyc@gmail.com>
2024-11-13 12:42:15 +0800

    Build frontend for ./LCEL/serve.py

--- LCEL/serve.py
+++ LCEL/serve.py
@@ -1,10 +1,14 @@
 #%% Import Libraries
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 from langserve import add_routes
+import time
+import threading
+import webbrowser
 import os
 from dotenv import load_dotenv
 
@@ -26,10 +30,21 @@ chain = prompt|llm|parser
 #%% Create App
 
 app = FastAPI(title = 'Langchain Server', version = "beta 0", description = 'A Simple API for Langchain Runable Interface')
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
 add_routes(app = app, runnable = chain, path = '/chain')
+
+#%% Open Browser
+
+def open_browser():
+    
+    time.sleep(1.5)
+    cwd = os.getcwd()
+    html_path = os.path.join(cwd, 'index.html')
+    webbrowser.open('file://' + html_path)
 
 #%% Execute App
 
 if __name__ == "__main__":
     import uvicorn
+    threading.Thread(target=open_browser).start()
     uvicorn.run(app = app, host = "127.0.0.1", port = 8000)
