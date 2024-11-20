46451c5cecf179a34d7f096479e336ff7fff613c
hawkiyc <hawkiyc@gmail.com>
2024-11-20 17:28:41 +0800

    Upload ./ChatBot/chatbot_with_ollama/app.py

--- ChatBot/chatbot_with_ollama/app.py
+++ ChatBot/chatbot_with_ollama/app.py
@@ -1,10 +1,11 @@
 #%% Import Libraries
 
 import streamlit as st
-import openai
-from langchain_openai import ChatOpenAI
+# import openai
+# from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
 import os
 from dotenv import load_dotenv
 
@@ -16,11 +17,9 @@ os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 if 'STREAMLIT_PUBLIC_PATH' in os.environ:
     os.environ['LANGCHAIN_API_KEY'] = st.secrets["LANGCHAIN_API_KEY"]
     os.environ['LANGCHAIN_PROJECT'] = st.secrets['LANGCHAIN_PROJECT']
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
 else:
     os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
     os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
-    openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #%% Prompt Template
 
@@ -32,7 +31,7 @@ prompt = ChatPromptTemplate.from_messages(
 
 def generate_reponse(question, model, temperature, max_tokens):
     
-    llm = ChatOpenAI(model = model)
+    llm = Ollama(model = model)
     output_parser = StrOutputParser()
     chain = prompt|llm|output_parser
     answer = chain.invoke({'question': question})
@@ -43,8 +42,8 @@ def generate_reponse(question, model, temperature, max_tokens):
 
 st.title('ChatBot with OpenAI')
 st.sidebar.title('Settings')
-llm_list = ['gpt-4o-mini', 'gpt-4o', 'o1-mini', 'gpt-3.5-turbo-0125']
-llm = st.sidebar.selectbox("Select Model", llm_list, index=llm_list.index('gpt-4o-mini'))
+llm_list = ['mistral', 'gemma2', 'phi3:medium', 'llama3.1', 'llama3.2']
+llm = st.sidebar.selectbox("Select Model", llm_list, index=llm_list.index('phi3:medium'))
 st.sidebar.write(f'Selected MOdel: {llm}')
 t = st.sidebar.slider('Temperature', min_value = 0.0, max_value = 1.0, value = .7)
 max_tokens = st.sidebar.slider('Max Tokens', min_value = 150, max_value = 500, value = 250)
