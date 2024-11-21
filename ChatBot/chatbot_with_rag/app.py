af2da38eccc1289aedaf2f42b93080786b6069d3
hawkiyc <hawkiyc@gmail.com>
2024-11-21 16:23:39 +0800

    Unfinished ./ChatBot/chat_with_rag/app.py

---
+++ ChatBot/chatbot_with_rag/app.py
@@ -0,0 +1,9 @@
+#%% Import Libraries
+
+import streamlit as st
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+
+#%%
